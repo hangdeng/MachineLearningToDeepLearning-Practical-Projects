dae7f8353dfc5643ae179139153ba1f183dad693
Hang Deng <hangdeng.geology@gmail.com>
2019-04-18 14:43:18 -0600

    Update helper.py

--- DeepLearning-Practical-Projects/project-tv-script-generation/helper.py
+++ DeepLearning-Practical-Projects/project-tv-script-generation/helper.py
@@ -11,7 +11,8 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, "r") as f:
+    # Use 'rU' instead of 'r' to avoid problem 
+    with open(input_file, "rU", encoding='utf-8') as f:
         data = f.read()
 
     return data
