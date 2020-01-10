8b2354216514868341f1aa8082b2311333fa11ea
Hang Deng <hade9202@colorado.edu>
2020-01-10 11:09:29 -0700

    Update gradientDescent.m

--- ML-Octave_Matlab/ml-ex1/ex1/gradientDescent.m
+++ ML-Octave_Matlab/ml-ex1/ex1/gradientDescent.m
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    error = X*theta - y; 
+    theta(1) = theta(1) - alpha*(1/m)*sum(error.*X(:,1));
+    theta(2) = theta(2) - alpha*(1/m)*sum(error.*X(:,2));
 
 
 
