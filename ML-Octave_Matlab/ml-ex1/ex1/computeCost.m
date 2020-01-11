243966a605419b03d933bdce416eb9caa2119ff1
Hang Deng <hade9202@colorado.edu>
2020-01-11 11:19:10 -0700

    Update computeCost.m

--- ML-Octave_Matlab/ml-ex1/ex1/computeCost.m
+++ ML-Octave_Matlab/ml-ex1/ex1/computeCost.m
@@ -12,8 +12,8 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-J = 1/(2*m)*sum(((X*theta)-y).^2);
+error = X*theta-y;
+J = 1/(2*m)*sum((error.^2);
 
 
 % =========================================================================
