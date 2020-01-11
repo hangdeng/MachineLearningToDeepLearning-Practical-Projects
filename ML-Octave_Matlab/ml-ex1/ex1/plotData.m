7d1e74084cd337ce939b0469008ca89eb7f0391e
Hang Deng <hade9202@colorado.edu>
2020-01-11 11:22:56 -0700

    Update plotData.m

--- ML-Octave_Matlab/ml-ex1/ex1/plotData.m
+++ ML-Octave_Matlab/ml-ex1/ex1/plotData.m
@@ -16,9 +16,9 @@ figure; % open a new figure window
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+xlabel('Profit in $10,000s');
+ylabel('Population of City in 10,000s');
 
 % ============================================================
 
