3cb26cb7f2b99ca3a927936848a175f13f28709f
Hang Deng <hade9202@colorado.edu>
2020-01-23 09:24:37 -0700

    Update costFunctionReg.m

--- ML-Octave_Matlab/ml-ex2/ex2/costFunctionReg.m
+++ ML-Octave_Matlab/ml-ex2/ex2/costFunctionReg.m
@@ -17,11 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% .* is element wise multiplication, * is matrix multiplication
+h = sigmoid(X*theta);
+J = (1/m)*sum(-y'*log(h)-(1-y)'*log(1-h))+lambda/(2*m)*sum(theta(2:end).^2);
+grad=1/m*X'*(h-y)+lambda/m*[0;theta(2:end)];
 
-
-
-
-
-% =============================================================
-
+% =============================================================0
 end
